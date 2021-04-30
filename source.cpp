5d9a91775115e074640fdf3353b8c0c0db9f6ba3
tesla707 <tesla707@mail.ru>
2021-05-01 01:08:33 +0300

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -1,8 +1,8 @@
 #include <d3d10_1.h>
 #include <wrl\client.h>
 
-#pragma comment (lib, "d3d10_1.lib")
-#pragma comment (lib, "dxgi.lib")
+#pragma comment (lib, "d3d10_1")
+#pragma comment (lib, "dxgi")
 
 using namespace Microsoft::WRL;
 
@@ -12,62 +12,62 @@ ComPtr<ID3D10RenderTargetView> renderTarget;
 ComPtr<IDXGIFactory> factory;
 ComPtr<IDXGISwapChain> swapChain;
 
-void KeyDown(UINT8 key) {
+void KeyDown(UINT8 key)
+{
 	if (GetAsyncKeyState(VK_ESCAPE))
 		exit(0);
 }
 
-void KeyUp(UINT8 key) {
+void KeyUp(UINT8 key)
+{
 
 }
 
-void Clear() {
+void Clear()
+{
 	device.ReleaseAndGetAddressOf();
 	renderTarget.ReleaseAndGetAddressOf();
 	factory.ReleaseAndGetAddressOf();
 	swapChain.ReleaseAndGetAddressOf();
 }
 
-void Update() {
+void Update()
+{
 
 }
 
-void Render() {
+void Render()
+{
 	float Color[4] = { 0.4f, 0.6f, 0.9f, 1.0f };
 	device->ClearRenderTargetView(renderTarget.Get(), Color);
 	swapChain->Present(1, 0);
 }
 
-void Init(_In_ HWND hWnd) {
+void Init(_In_ HWND hWnd)
+{
 #if defined(DEBUG) | defined(_DEBUG)
 	UINT flag = D3D10_CREATE_DEVICE_DEBUG;
 #else
 	UINT flag = D3D10_CREATE_DEVICE_SINGLETHREADED | D3D10_CREATE_DEVICE_BGRA_SUPPORT;
 #endif
 
-	if (SUCCEEDED(D3D10CreateDevice1(nullptr, D3D10_DRIVER_TYPE_HARDWARE, nullptr,
-		flag, D3D10_FEATURE_LEVEL_10_1, D3D10_1_SDK_VERSION, &device)))
+	if (SUCCEEDED(D3D10CreateDevice1(nullptr, D3D10_DRIVER_TYPE_HARDWARE, nullptr, flag, D3D10_FEATURE_LEVEL_10_1, D3D10_1_SDK_VERSION, &device)))
 		goto Continue;
-	else if (SUCCEEDED(D3D10CreateDevice1(nullptr, D3D10_DRIVER_TYPE_HARDWARE, nullptr,
-		flag, D3D10_FEATURE_LEVEL_10_0, D3D10_1_SDK_VERSION, &device)))
+	else if (SUCCEEDED(D3D10CreateDevice1(nullptr, D3D10_DRIVER_TYPE_HARDWARE, nullptr, flag, D3D10_FEATURE_LEVEL_10_0, D3D10_1_SDK_VERSION, &device)))
 		goto Continue;
-	else if (SUCCEEDED(D3D10CreateDevice1(nullptr, D3D10_DRIVER_TYPE_HARDWARE, nullptr,
-		flag, D3D10_FEATURE_LEVEL_9_3, D3D10_1_SDK_VERSION, &device)))
+	else if (SUCCEEDED(D3D10CreateDevice1(nullptr, D3D10_DRIVER_TYPE_HARDWARE, nullptr, flag, D3D10_FEATURE_LEVEL_9_3, D3D10_1_SDK_VERSION, &device)))
 		goto Continue;
-	else if (SUCCEEDED(D3D10CreateDevice1(nullptr, D3D10_DRIVER_TYPE_HARDWARE, nullptr,
-		flag, D3D10_FEATURE_LEVEL_9_2, D3D10_1_SDK_VERSION, &device)))
+	else if (SUCCEEDED(D3D10CreateDevice1(nullptr, D3D10_DRIVER_TYPE_HARDWARE, nullptr, flag, D3D10_FEATURE_LEVEL_9_2, D3D10_1_SDK_VERSION, &device)))
 		goto Continue;
-	else if (SUCCEEDED(D3D10CreateDevice1(nullptr, D3D10_DRIVER_TYPE_HARDWARE, nullptr,
-		flag, D3D10_FEATURE_LEVEL_9_1, D3D10_1_SDK_VERSION, &device)))
+	else if (SUCCEEDED(D3D10CreateDevice1(nullptr, D3D10_DRIVER_TYPE_HARDWARE, nullptr, flag, D3D10_FEATURE_LEVEL_9_1, D3D10_1_SDK_VERSION, &device)))
 		goto Continue;
 	else {
-		MessageBox(NULL, "Your GPU doesn't support D3D10_FEATURE_LEVEL_9_1 or higher.",
-			"Error", MB_ICONERROR);
+		MessageBox(nullptr, TEXT("Your GPU doesn't support D3D10_FEATURE_LEVEL_9_1 or higher."), TEXT("Error"), MB_ICONERROR);
 		exit(-1);
 	}
 
 Continue:
-	DXGI_SWAP_CHAIN_DESC desc = {};
+	DXGI_SWAP_CHAIN_DESC desc{};
 	desc.BufferCount = 2;
 	desc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
 	desc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
@@ -80,13 +80,15 @@ Continue:
 	factory->CreateSwapChain(device.Get(), &desc, &swapChain);
 
 	ComPtr<ID3D10Resource> resource;
-	swapChain->GetBuffer(0, IID_PPV_ARGS(&resource));
+	swapChain->GetBuffer(NULL, IID_PPV_ARGS(&resource));
 	device->CreateRenderTargetView(resource.Get(), nullptr, &renderTarget);
 	resource.ReleaseAndGetAddressOf();
 }
 
-LRESULT CALLBACK WindowProc(_In_ HWND hWnd, _In_ UINT uMsg, _In_ WPARAM wParam, _In_ LPARAM lParam) {
-	switch (uMsg) {
+LRESULT CALLBACK WindowProc(_In_ HWND hWnd, _In_ UINT message, _In_ WPARAM wParam, _In_ LPARAM lParam)
+{
+	switch (message)
+	{
 	case WM_CREATE:
 		Init(hWnd);
 		return 0;
@@ -108,31 +110,39 @@ LRESULT CALLBACK WindowProc(_In_ HWND hWnd, _In_ UINT uMsg, _In_ WPARAM wParam, 
 		Clear();
 		PostQuitMessage(0);
 		return 0;
+
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
 	}
-	return DefWindowProc(hWnd, uMsg, wParam, lParam);
+
+	return 0;
 }
 
-int CALLBACK WinMain(_In_ HINSTANCE hInstance, _In_ HINSTANCE hPrevInstance,
-	_In_ LPSTR lpCmdLine, _In_ int nCmdShow) {
-	WNDCLASSEX wc = {};
-	wc.cbSize = sizeof(WNDCLASSEX);
+int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ PTCHAR lpCmdLine, _In_ int nShowCmd)
+{
+	WNDCLASS wc{};
 	wc.style = CS_HREDRAW | CS_VREDRAW;
 	wc.lpfnWndProc = WindowProc;
 	wc.hInstance = hInstance;
-	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wc.lpszClassName = "WindowClass";
-	RegisterClassEx(&wc);
-	HWND hWnd = CreateWindow(wc.lpszClassName, "Test DirectX 10 for D3D10CreateDevice1",
+	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wc.lpszClassName = TEXT("WindowClass");
+	RegisterClass(&wc);
+
+	auto hWnd = CreateWindow(wc.lpszClassName, TEXT("Test DirectX 10 for D3D10CreateDevice1"),
 		WS_SYSMENU | WS_MINIMIZEBOX,
 		(GetSystemMetrics(SM_CXSCREEN) - 1024) / 2,
 		(GetSystemMetrics(SM_CYSCREEN) - 576) / 2,
-		1024, 576, nullptr, nullptr, hInstance, 0);
-	ShowWindow(hWnd, nCmdShow);
-	MSG msg = {};
+		1024, 576, nullptr, nullptr, hInstance, NULL);
+	
+	ShowWindow(hWnd, nShowCmd);
+	UpdateWindow(hWnd);
+
+	MSG msg{};
 	while (msg.message != WM_QUIT)
-		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+		if (PeekMessage(&msg, nullptr, NULL, NULL, PM_REMOVE)) {
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);
 		}
-	return static_cast<char>(msg.wParam);
+
+	return (int)msg.wParam;
 }
